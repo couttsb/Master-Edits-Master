630ac97b6db69535b971ec571730aaa520fa25a5
couttsb <brent.coutts@mail.mcgill.ca>
2016-09-08 01:17:14 -0400

    Let's go to master

--- src/MasterEditsMaster.java
+++ src/MasterEditsMaster.java
@@ -1,6 +1,6 @@
 
 public class MasterEditsMaster {
 	public static void main(String[] args) {
-		System.out.println("I'm tired so we are going to go back to the master list!");
+		System.out.println("I'm tired so we are going to go back to the master list! For real now.");
 	}
 }
